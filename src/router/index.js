ce17c072c9e8cb0d170ead4c3b2dc1b2b424694c
kang <1243936032@qq.com>
2019-05-14 20:59:55 +0800

    删除多余的文件

--- src/router/index.js
+++ src/router/index.js
@@ -26,11 +26,6 @@ export default new Router({
                     component: resolve => require(['../components/page/VueTable.vue'], resolve)     // vue-datasource组件
                 },
                 {
-                    path: '/editusertable',
-                    name: 'editusertable',
-                    component: resolve => require(['../components/page/EditUserTable.vue'], resolve)     // vue-datasource组件
-                },
-                {
                     path: '/addemployee',
                     name: 'addemployee',
                     component: resolve => require(['../components/page/AddEmploy.vue'], resolve)     // vue-datasource组件
@@ -47,22 +42,6 @@ export default new Router({
                 {
                     path: '/memberList',
                     component: resolve => require(['../components/page/VueEditor.vue'], resolve)    // Vue-Quill-Editor组件
-                },
-                {
-                    path: '/markdown',
-                    component: resolve => require(['../components/page/Markdown.vue'], resolve)     // Vue-Quill-Editor组件
-                },
-                {
-                    path: '/upload',
-                    component: resolve => require(['../components/page/Upload.vue'], resolve)       // Vue-Core-Image-Upload组件
-                },
-                {
-                    path: '/basecharts',
-                    component: resolve => require(['../components/page/BaseCharts.vue'], resolve)   // vue-schart组件
-                },
-                {
-                    path: '/drag',
-                    component: resolve => require(['../components/page/DragList.vue'], resolve)    // 拖拽列表组件
                 },
                 {
                     path: '/findBill',
